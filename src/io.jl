ac0a7ed2b045b95c64a464a7b4082e466e1a9800
lambcount <53368935+lambcount@users.noreply.github.com>
2021-03-28 13:13:32 +0200

    API functions

--- src/io.jl
+++ src/io.jl
@@ -47,10 +47,82 @@ function ec_grab(measurement::AbstractString; dir::AbstractString = "./Data/EC")
 
 end
 
-function ec_list(; dir::AbstractString = "./Data/EC")
+function ec_list(;   dir::AbstractString = "./Data/EC",
+                     scinote::Bool = true,
+                     inexact::AbstractString  = "",
+                     measurement::AbstractString = "",
+                     scan_rate = "",
+                     pump::AbstractString = ""
+                    )
+                    
     dirs_files = readdir(dir,join=true)
     files = [dirs_files[i] for i in 1:length(dirs_files) if isdir(dirs_files[i]) == false]
     filenames = first.(splitext.(last.(split.(files,"\\"))))
     
     return DataFrame(Measurement = filenames)
+end
+
+
+"""
+    html2string(html)
+
+Convert a html to a normal String.
+"""
+function html2string(html)
+    div_start = "<div>"
+    div_end = "</div>"
+    space = "&nbsp;"
+    
+    _str = replace(html,div_start=>"") 
+    _str = replace(_str,div_end=>"") 
+    _str = replace(_str,space=>"") 
+            
+    return _str
+    
+end
+
+"""
+comment2measurement(str)
+Extract Measurment and other comments from string.
+"""
+function comment2measurement(str)
+
+    _idx = findall(x-> isspace(x)==true,str)
+
+    if isempty(_idx)==true
+        measurement = str
+        pump = ""
+        rest = ""
+        return measurement,pump,rest
+    else
+        idx_start=[1;[i for i in 2:length(str) if (isprint(str[i]) == true) && (isspace(str[i-1])==true)]]
+        idx_end=[[i for i in 1:length(str)-1 if (isprint(str[i]) == true) && (isspace(str[i+1])==true)];length(str)]
+
+        if length(idx_start) == 1
+
+            measurement = str[idx_start[1]:idx_end[1]]
+            pump = ""
+            rest= ""
+
+            return measurement,pump,rest
+
+        elseif length(idx_divs) == 2
+
+            measurement = str[idx_start[1]:idx_end[1]]
+            pump = str[idx_start[2]:idx_end[2]]
+            rest = ""
+
+            return measurement,pump,rest
+
+        elseif length(idx_divs) > 2
+
+            measurement = str[idx_start[1]:idx_end[1]]
+            pump = str[idx_start[2]:idx_end[2]]
+            rest = str[idx_start[3]:end]
+
+            return measurement,pump,rest
+        end
+
+    end
+
 end
--- src/scinote.jl
+++ src/scinote.jl
@@ -1,25 +1,43 @@
-using HTTP,JSON
+using HTTP,JSON,DataFrames
 
 const server_name = "http://titus.phchem.uni-due.de:3000"
-const client_id = ""
-const client_secret = ""
+const client_id = "mp1YyqtADkI8wHqbRyAEsqdr3hALhjRqYgsw5T5snM8"
+const client_secret = "mmtmPd9ctROk83hKk21qECDTtlwzsbhMF7pHob601LY"
 const redirect_uri = ""
 const team_id = "AK Hasselbrink"
 const project_id_femto_lab = "9"
 const experiment_id_SEC = "53"
 const task_id_EC = "297"
 
+const server_name_home = "http://192.168.178.58:3000"
+const client_id_home = "vBNWRS3xi_d4QMm7McGde7kJhmE85ssLYes_7iab_Tw"
+const client_secret_home = "ox1WawrGtiQgcRRaAAC5vAHDvlY8D_oOs0uoneHhk4k"
 
 
+#a=HTTP.post("http://titus.phchem.uni-due.de:3000/oauth/token?grant_type=password&email=tim.laemmerzahl@uni-due.de&password=1a2s3d4f&client_id=mp1YyqtADkI8wHqbRyAEsqdr3hALhjRqYgsw5T5snM8&client_secret=mmtmPd9ctROk83hKk21qECDTtlwzsbhMF7pHob601LY")
+
+#function auth_code()
+#    r = HTTP.request("GET",server_name*"/oauth/authorize?client_id=mp1YyqtADkI8wHqbRyAEsqdr3hALhjRqYgsw5T5snM8&redirect_uri=&response_type=code")
+#    String(r.body)
+#    
+#end 
+
+#authorization_code = auth_code()
 
 
-function auth_code()
-    r = HTTP.request("GET",server_name*"/oauth/authorize?client_id=tim.laemmerzahl@uni-due.de&redirect_uri=$(server_name)&response_type=code")
-    String(r.body)
-    
-end 
-
-authorization_code = auth_code()
+"""
+    api_running()
+Returns if the API is running. 
+"""
+function api_running()
+    r = HTTP.request("GET",server_name_home*"/api/health")
+    resp = String(r.body)
+    if (resp == "RUNNING") == true
+        return true
+    else
+        error("The API seems not to be running. Get Request returns $(resp)")
+    end
+end
 
 """
 token()
@@ -27,36 +45,128 @@ token()
 Get token with client_id, client_secret, authorization_code, and redirect_uri.
 """
 function token()
-    resp = HTTP.request("POST", SCINOTE_URL*"/oauth/token",
-        ["grant_type" => "authorization_code",
-        "client_id" => client_id,
-        "client_secret" => client_secret,
-        "code" => authorization_code,
-        "redirect_uri" => redirect_uri
-        ])
-    body = String(resp.body)
-    JSON.parse(body)
+    if api_running() == true
+        header = Dict("Content-Type"=> "application/json")
+        params = Dict(
+            "grant_type" => "password",
+            "email" => "tim.laemmerzahl@uni-due.de",
+            "password"=> "1a2s3d4f",
+            "client_id" => client_id_home,
+            "client_secret" => client_secret_home
+        )
+        resp = HTTP.request("POST", server_name_home*"/oauth/token",header,JSON.json(params))
+        body = String(resp.body)
+        return JSON.parse(body)["access_token"]
+    end
 end
 
+access_token = token()
+header= Dict("Authorization"=> "Bearer $(access_token)")
+
+
 """
-refreshtoken()
-
-Refresht the  token with client_id, client_secret, authorization_code, and redirect_uri.
+Returns the Status of the Scinote API
 """
-function refreshtoken()
-    resp = HTTP.request("POST", SCINOTE_URL*"/oauth/token",
-        ["grant_type" => "refresh_token",
-        "client_id" => client_id,
-        "client_secret" => client_secret,
-        "refresh_token" => token,
-        "redirect_uri" => redirect_uri
-        ])
-    body = String(resp.body)
-    JSON.parse(body)
-end
-
-
 function api_status()
-    r = HTTP.request("GET",server_name*"/api/status")
+    r = HTTP.request("GET",server_name_home*"/api/status",header)
     JSON.parse(String(r.body))
+end
+
+
+"""
+    get_teams()
+This function retrieves all teams user is member of.   
+"""
+function get_teams()
+    if api_running() == true 
+        resp= HTTP.request("GET",server_name_home*"/api/v1/teams",header)
+        body = String(resp.body)
+        data = JSON.parse(body)["data"]            
+    end
+end
+
+"""
+    get_projects(team::Int64)
+This function retrieves all projects from the AK Hasselbrink team.
+"""
+function get_projects(team::Int64)
+    if api_running() == true 
+        resp=HTTP.request("GET",server_name_home*"/api/v1/teams/$(team)/projects",header)
+        body = String(resp.body)
+        data=JSON.parse(body)["data"]
+    end 
+end
+
+"""
+    get_experiments(project::Int64,team::Int64)
+This function retrieves all experiments from the specified project
+"""
+function get_experiments(team::Int64,project::Int64)
+    if api_running() == true 
+        resp=HTTP.request("GET",server_name_home*"/api/v1/teams/$(team)/projects/$(project)/experiments",header)
+        body = String(resp.body)
+        data=JSON.parse(body)["data"] 
+    end
+end
+
+"""
+    get_tasks(team::Int64,project::Int64,experiment::Int64)
+This function retrieves all tasks from a specific experiment. 
+"""
+function get_tasks(team::Int64,project::Int64,experiment::Int64)
+    if api_running() == true 
+        resp=HTTP.request("GET",server_name_home*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks",header)
+        body = String(resp.body)
+        data=JSON.parse(body)["data"]
+    end
+end
+
+
+"""
+    get_protocols(team::Int64,project::Int64,experiment::Int64,task::Int64)
+This function retrieves all protocols from a specific experiment. 
+"""
+function get_protocols(team::Int64,project::Int64,experiment::Int64,task::Int64)
+    if api_running() == true 
+        resp=HTTP.request("GET",server_name_home*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks/$(task)/protocols",header)
+        body = String(resp.body)
+        data=JSON.parse(body)["data"]
+    end
+end
+
+
+"""
+    get_steps(team::Int64,project::Int64,experiment::Int64,task::Int64,protocol::Int64)
+This function retrieves all steps from a specific protocol. 
+"""
+function get_steps(team::Int64,project::Int64,experiment::Int64,task::Int64,protocol::Int64)
+    if api_running() == true 
+        resp=HTTP.request("GET",server_name_home*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks/$(task)/protocols/$(protocol)/steps",header)
+        body = String(resp.body)
+        data=JSON.parse(body)["data"]
+        #df = DataFrame()
+        #df.Names  = [data[i]["attributes"]["name"] for i in 1:length(data)]
+        #df.Comment = [data[i]["attributes"]["description"] for i in 1:length(data)]
+        #return df
+
+    end
+end
+
+"""
+    get_step_table(team::Int64,project::Int64,experiment::Int64,task::Int64,protocol::Int64,step::Int64)
+This function retrieves the table from specific step. 
+Empty cells will be ignored. Be sure to have a proper 
+"""
+function get_step_table(team::Int64,project::Int64,experiment::Int64,task::Int64,protocol::Int64,step::Int64)
+    if api_running() == true
+        resp=HTTP.request("GET",server_name_home*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks/$(task)/protocols/$(protocol)/steps/$(step)/tables",header)
+        body = String(resp.body)
+        data=JSON.parse(JSON.parse(body)["data"][1]["attributes"]["contents"])["data"]
+        df = DataFrame()
+            df.Parameter = [data[i][1] for i in 1:length(data) if data[i][1] !== nothing]
+            df.Values = [data[i][2] for i in 1:length(data) if data[i][1] !== nothing]
+            df.Uni = [data[i][3] for i in 1:length(data) if data[i][1] !== nothing]
+
+            return df
+    end
 end
