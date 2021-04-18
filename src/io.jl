62b63e76f80176e4f1e26993620460ce95b6029f
lambcount <53368935+lambcount@users.noreply.github.com>
2021-04-18 13:03:38 +0200

    Filters for ec_list and added ec_grab for DataFrame

--- src/io.jl
+++ src/io.jl
@@ -1,11 +1,25 @@
 using HDF5
 import Base: findall
+
+function get_fnames_and_types(filepath)
+    filenames = first.(splitext.(last.(split.(filepath,'/'))))
+    fileext = last.(splitext.(last.(split.(filepath,'/'))))
+
+    return filenames,fileext
+end
 """
     ec_grab(measurement::AbstractString; dir::AbstractString = "./Data/EC")
 
 Fetch the CV and OCP Measurements from your dir. 
 
-eg. v,c = ec_grab("001-001",dir = "..")
+eg. 
+    julia> v,c = ec_grab("001-001",dir = "..")\n\t
+
+
+You can also grab the DataFrame from ec_list(). Be sure that the experiments in ec_list() all are either CV or OCP or another experiment. Otherwise this will result in an error.
+    julia>v=ec_grab(ec_list(measurement="OCP"))
+    
+    julia>v=ec_grab(ec_list(measurement="CV"))
 """
 function ec_grab(measurement::AbstractString; dir::AbstractString = "./Data/EC")
     if occursin("EC-",measurement) == false
@@ -16,8 +30,7 @@ function ec_grab(measurement::AbstractString; dir::AbstractString = "./Data/EC")
 
         dirs_files = readdir(dir,join=true)
         files = [dirs_files[i] for i in 1:length(dirs_files) if isdir(dirs_files[i]) == false]
-        filenames = first.(splitext.(last.(split.(files,'/'))))
-        fileext = last.(splitext.(last.(split.(files,'/'))))
+        filenames,fileext =get_fnames_and_types(files)
 
         idxs = findall(x-> x==(_measurement),filenames)
 
@@ -68,6 +81,49 @@ function ec_grab(measurement::AbstractString; dir::AbstractString = "./Data/EC")
 
 
 end
+
+function ec_grab(df::DataFrame)
+
+    path = df[:,end]
+    fileext= get_fnames_and_types(path)[2]
+
+    data=[]
+    for i in 1:first(size(df))
+        if fileext[i] == ".h5"
+            _measurement = get_measurement(path[i])
+            _data = h5open(path[i]) do fid
+                read(fid[_measurement]["Data"])
+            end
+            push!(data,_data)
+        elseif fileext[i] == ".csv"
+            _data = readdlm(path[i])
+            push!(data,_data)
+        elseif fileext[i] == ".txt"
+            _data = readdlm(path[i])[:,1:2]
+            push!(data,_data)
+        end
+    end
+
+    if all(last.(size.(data)).== last(size(data[1])))
+        if (last(size(data[1])) == 1) || (try size(data[1])[2] catch end === nothing)
+            _dim1 = [data[i][:,1] for i in 1:length(data)]
+            @show data[1]
+            return _dim1
+        elseif last(size(data[1])) == 2
+            _dim1 = [data[i][:,1] for i in 1:length(data)]
+            _dim2 = [data[i][:,2] for i in 1:length(data)]
+            return _dim1,_dim2
+        end
+    else
+        error("The Files you selected in ec_list() are not compatible.")
+    end
+    
+
+    
+
+    
+end
+
 
 
 """
@@ -131,32 +187,62 @@ end
 
 
 """
-Returns the List of all Measurements in dir. Use kwargs to filter the df.
+Returns the List of all Measurements in dir. Use kwargs to filter the df. 
+Use show_na to show older measurements without parameters.\t
+
     function ec_list(;   dir::AbstractString = "./test/Data/EC",
         inexact::AbstractString  = "",
         measurement::AbstractString = "",
         scan_rate = "",
+        show_na=false
    )
 
-e.g.
+e.g.\t\n
     julia> ec_list()
-    4×3 DataFrame
-     Row │ Filename     Measurment  ScanRate 
-         │ String       String      Any      
-    ─────┼───────────────────────────────────
-       1 │ EC-001-001   n/A         n/A
-       2 │ EC-XXX-XXX   n/A         n/A
-       3 │ EC-test_CV   CV          50.0
-       4 │ EC-test_OCP  OCP         n/A
-    
-    >julia ec_list(inexact="001-001)
+    2×4 DataFrame
+    Row │ Filename     Measurement  ScanRate  Path                          
+        │ String       String       Any       String                        
+   ─────┼─────────────────────────────────────────────────────────────────── \n
+      1 │ EC-test_CV   CV           50.0      ./test/Data/EC/EC-test_CV.h5
+      2 │ EC-test_OCP  OCP          n/A       ./test/Data/EC/EC-test_OCP.h5
+
+
+
+    julia> ec_list(show_na=true)
+    4×4 DataFrame
+    Row │ Filename     Measurement  ScanRate  Path                          
+        │ String       String       Any       String                        
+   ─────┼───────────────────────────────────────────────────────────────────\n
+      1 │ EC-001-001   n/A          n/A       ./test/Data/EC/EC-001-001.txt
+      2 │ EC-XXX-XXX   n/A          n/A       ./test/Data/EC/EC-XXX-XXX.csv
+      3 │ EC-test_CV   CV           50.0      ./test/Data/EC/EC-test_CV.h5
+      4 │ EC-test_OCP  OCP          n/A       ./test/Data/EC/EC-test_OCP.h5
+
+
+       
+    julia> ec_list(inexact="001-001",show_na=true)
+    1×4 DataFrame
+    Row │ Filename    Measurement  ScanRate  Path                          
+        │ String      String       Any       String                        
+   ─────┼──────────────────────────────────────────────────────────────────\n
+      1 │ EC-001-001  n/A          n/A       ./test/Data/EC/EC-001-001.txt
+
+
+
+
+    julia> ec_list(scan_rate=50)
+    1×4 DataFrame
+    Row │ Filename    Measurement  ScanRate  Path                         
+        │ String      String       Any       String                       
+   ─────┼─────────────────────────────────────────────────────────────────\n
+      1 │ EC-test_CV  CV           50.0      ./test/Data/EC/EC-test_CV.h5
 """
 function ec_list(;   dir::AbstractString = "./test/Data/EC",
                      inexact::AbstractString  = "",
                      measurement::AbstractString = "",
                      scan_rate = missing ,
                      #pump::AbstractString = "",
-                     ex_na = false
+                     show_na = false
                     )
                     
     dirs_files = readdir(dir,join=true)
@@ -171,15 +257,34 @@ function ec_list(;   dir::AbstractString = "./test/Data/EC",
 
     measurements = [filetype[i] == ".h5" ? get_measurement(files[i]) : "n/A" for i in 1:length(files)]
     scan_rates = [(filetype[i] == ".h5" && get_measurement(files[i]) == "CV") ? get_params(files[i])[2,2] : "n/A" for i in 1:length(files)]
-    df = DataFrame(Filename = filenames, Measurement = measurements, ScanRate= scan_rates)
+    _df = DataFrame(Filename = filenames, Measurement = measurements, ScanRate= scan_rates, Path=files)
 
-   #if ex_na == false
-   #   return  df[((df.Measurement .== measurement) .| (df.Measurement .== "n/A")),:]
-   #elseif ex_na == true
-   #    return  df[(df.Measurement .== measurement),:]
+   if show_na == false
+        _df=_df[(_df.Measurement .!== "n/A"),:]
+    if inexact !== ""
+        _df=_df[(occursin.(inexact,_df.Filename) .== true ),:]
+    end
+    if measurement !== ""
+        _df = _df[(_df.Measurement .== measurement),:]
+    end
+    if scan_rate !== missing
+        _df = _df[(_df.ScanRate .== scan_rate),:]
+    end
+    return _df
 
-    #end
+   elseif show_na == true
+    if inexact !== ""
+        _df=_df[(occursin.(inexact,_df.Filename) .== true ),:]
+    end
+    if measurement !== ""
+        _df = _df[((_df.Measurement .== measurement) .| (_df.Measurement .== "n/A")),:]
+    end
+    if scan_rate !== missing
+        _df = _df[(((_df.ScanRate .== scan_rate) .| (_df.ScanRate .== "n/A")) .& (_df.Measurement .== "CV" )),:]
+    end
+        return _df  
 
+    end
 
 end
 
