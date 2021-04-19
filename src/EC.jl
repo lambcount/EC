84bd20df8e7a0f46e790c184faf98f2bf067fc3d
lambcount <53368935+lambcount@users.noreply.github.com>
2021-04-19 17:52:41 +0200

    get_curr_range()

--- src/EC.jl
+++ src/EC.jl
@@ -5,12 +5,15 @@ using DelimitedFiles,DataFrames
 
 include("./data.jl")
 include("./io.jl")
+include("./struct.jl")
 
 export
+    Measurement,
     ec_grab,
     ec_list,
     cycle,
     total_cycles,
     get_measurement,
-    get_params
+    get_params,
+    get_curr_range,
 end
--- src/struct.jl
+++ src/struct.jl
@@ -1,13 +1,15 @@
 import Base: +,-,/,*,==,!==
+import Plots: plot
 
 mutable struct Measurement
     Name::AbstractString
-    Data::Array
+    Data
     Labbook::Dict{Any,Any}
 end
 
 Base.size(m::Measurement) = size(m.Data)
 Base.copy(m::Measurement)  = Measurement(copy(m.Name),copy(m.Data),copy(m.Labbook))
+Plots.plot(m::Measurement) = plot(m.Data[:,1],m.Data[:,2])
 
 ==(m1::Measurement,m2::Measurement) = (m1.Name == m2.Name) && (m1.Data == m2.Data)
 !==(m1::Measurement,m2::Measurement) = (m1.Name !== m2.Name) || (m1.Data !== m2.Data)
@@ -17,4 +19,5 @@ Base.copy(m::Measurement)  = Measurement(copy(m.Name),copy(m.Data),copy(m.Labboo
 *(m::Measurement,n::Number) = n * m.Data 
 /(m::Measurement,n::Number) = m.Data /n
 /(m::Measurement,n::Number) = n / m.Data 
+
 
