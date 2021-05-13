0b9ca30fbc0adf232a1a5c9eeb201478cad68988
lambcount <53368935+lambcount@users.noreply.github.com>
2021-05-13 12:27:19 +0200

    anodic/cathodic current
    Added a function for determining the anodic and cathdoic current at a given potential.

--- src/data.jl
+++ src/data.jl
@@ -1,3 +1,5 @@
+import SmoothingSplines: fit,predict
+
 function idx_cycle(voltage)
 
     _voltage = voltage .+1000
@@ -56,4 +58,94 @@ function total_cycles(voltage)
 
     idx = idx_cycle(voltage)
     total_cycle = round(length(voltage)/idx) |> Int
+
+    return total_cycle
 end  
+
+
+
+"""Returns the anodic and cathodic current at the given potential value (val).Works only if c and v consist of only one cycle. For Value you can put any potential in the scan range.
+    get_eq_anodickathodicpotentials(v,c,val)
+
+julia> get_eq_anodickathodicpotentials(v_cycle1[1],c_cycle1[1],-0.03)
+    (-1.3481340532268844, 0.9398593965871291)
+"""
+function get_eq_anodickathodicpotentials(v,c,val)
+    idx_1,idx_2 = get_ind_eq_anodickathodicpotentials(v,c,val)
+
+    val_p1 = v[idx_1]
+    val_p2 = v[idx_2]
+    val_p3 = v[idx_2+1]
+
+    inc_idx = abs(val_p1-val_p2 )/abs(val_p3-val_p2)
+
+    itp = interpolate(c, BSpline(Quadratic(Reflect(OnCell()))))
+
+    if val_p1 < val_p2 
+        
+        return c[idx_1],itp(idx_2+inc_idx)
+
+    elseif val_p1 > val_p2 
+        
+        return c[idx_1],itp(idx_2+(1-inc_idx))
+    end 
+end
+
+
+"""Returns the anodic and cathodic current indices at the given potential value (val).Works only if c and v consist of only one cycle. For Value you can put any potential in the scan range.
+get_ind_eq_anodickathodicpotentials(v,c,val)
+
+julia> get_eq_anodickathodicpotentials(v_cycle1[1],c_cycle1[1],-0.03)
+    (42,97)
+"""
+function get_ind_eq_anodickathodicpotentials(v,c,val)
+
+    p=sortperm(v)
+    v_sorted = v[p]
+
+    _v = v_sorted .- val
+
+    idx_1=findnext(x->x>= 0,_v,1)
+
+    idx_2=findpotential(v,p,idx_1,c)
+
+    idxs= [p[idx_1],p[idx_2]]
+
+    return idxs
+
+    
+end
+
+
+function findpotential(v,p,idx,c;sample_rate=10)
+    val = v[p[idx]]
+    pot_halfcycle= abs(maximum(v) - minimum(v))
+
+    time4halfcycle=  abs((argmin(v) - argmax(v))/sample_rate)
+
+    scan_rate = pot_halfcycle/time4halfcycle
+
+    if c[p[idx]] > 0 
+        current= "anodic"
+    else
+        current= "cathodic"
+    end
+
+    idx_2= 0
+
+    for i in idx-10:idx+10
+        if current == "anodic"
+            if abs(v[p][i] - val) < (scan_rate/sample_rate) && c[p[i]] < 0 && c[p[i]] < 0
+                idx_2= i
+                return idx_2
+            end
+        elseif current == "cathodic"
+            if abs(v[p][i] - val) < (scan_rate/sample_rate) && c[p[i]] > 0
+                idx_2= i
+                return idx_2
+            end
+        end
+    end
+
+  
+end
