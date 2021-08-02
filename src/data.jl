abfef8d993e610415fd5e69f8ac6eb6e484b3dc2
lambcount <53368935+lambcount@users.noreply.github.com>
2021-08-02 19:45:49 +0200

    cycle fixed

--- src/data.jl
+++ src/data.jl
@@ -1,32 +1,67 @@
-function idx_cycle(voltage)
+
+
+
+function idx_cycle(voltage,start_idx)
 
     _voltage = voltage .+1000
-    if _voltage[1] > _voltage[10]
-        _idx=findnext(x->x>= _voltage[1],_voltage,2)
-        idx=findnext(x->x<= _voltage[1],_voltage,_idx+1)
 
-    elseif _voltage[1] < _voltage[10]
-        _idx=findnext(x->x<= _voltage[1],_voltage,2)
-        idx=findnext(x->x>= _voltage[1],_voltage,_idx+1)
+    if _voltage[start_idx] > _voltage[start_idx+10]
+        _idx=findnext(x->x>= _voltage[start_idx],_voltage,start_idx+1)
+        idx=findnext(x->x<= _voltage[start_idx],_voltage,_idx+1)
+
+    elseif _voltage[start_idx] < _voltage[start_idx+10]
+        _idx=findnext(x->x<= _voltage[start_idx],_voltage,start_idx+1)
+        idx=findnext(x->x>= _voltage[start_idx],_voltage,_idx+1)
     end
-    return idx+5
+    return idx
 end
 
+
+"""
+total_cycles(voltage::Array{Number,1})
+
+Returns the total number of full cycles in voltage.
+"""
+function total_cycles(voltage)
+
+    idx = idx_cycle(voltage,1)
+    total_cycle = round(length(voltage)/idx) |> Int
+end  
 
 
 
 function cycle(voltage,cycle::Int64) 
 
-    idx = idx_cycle(voltage)
-    total_cycle = round(length(voltage)/idx) |> Int
     
-    if cycle > total_cycle
-        error("There are only $(total_cycle) full cycles in your Array.")
+    idx = idx_cycle(voltage,1)
+    total_cycles = round(length(voltage)/idx) |> Int
+   
+    if cycle > total_cycles
+        error("There are only $(total_cycles) full cycles in your Array.")
     else
-        _start = 1+(cycle-1)*idx
-        _end = cycle*idx
-        return voltage[_start:_end]
-    end      
+
+        if cycle == 1
+        
+            return voltage[1:idx]
+
+        elseif cycle > 1 
+            _idx_1 = 1
+            _idx_2 = deepcopy(idx)
+    
+
+            for i in 2:cycle
+
+                if i == cycle
+                    _idx_1 = deepcopy(_idx_2)
+                end
+                _idx_2 = idx_cycle(voltage,_idx_2)
+
+                
+          
+            end
+            return voltage[_idx_1:_idx_2]
+        end
+    end       
 end
 
 """
@@ -36,27 +71,31 @@ Return the Input voltage and current Arrays for the given cycle argument.
 """
 function cycle(voltage,current,cycle::Int64)
 
-    idx = idx_cycle(voltage)
-    total_cycle = round(length(voltage)/idx) |> Int
-    
-    if cycle > total_cycle
-        error("There are only $(total_cycle) full cycles in your Array.")
+    idx = idx_cycle(voltage,1)
+    total_cycles = round(length(voltage)/idx) |> Int
+   
+    if cycle > total_cycles
+        error("There are only $(total_cycles) full cycles in your Array.")
     else
-        _start = 1+(cycle-1)*idx
-        _end   = cycle*idx
 
-        return voltage[_start:_end],current[_start:_end]
+        if cycle == 1
+        
+            return voltage[1:idx],current[1:idx]
 
-    end 
-end 
+        elseif cycle > 1 
+            _idx_1 = 1
+            _idx_2 = deepcopy(idx)
+    
 
-"""
-total_cycles(voltage::Array{Number,1})
+            for i in 2:cycle
 
-Returns the total number of full cycles in voltage.
-"""
-function total_cycles(voltage)
+                if i == cycle
+                    _idx_1 = deepcopy(_idx_2)
+                end
 
-    idx = idx_cycle(voltage)
-    total_cycle = round(length(voltage)/idx) |> Int
-end  
+                _idx_2 = idx_cycle(voltage,_idx_2)
+            end
+            return voltage[_idx_1:_idx_2],current[_idx_1:_idx_2]
+        end
+    end  
+end
