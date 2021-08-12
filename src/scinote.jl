632ffbe043cf216a629f68d948f47d8df4f72d41
lambcount <53368935+lambcount@users.noreply.github.com>
2021-08-12 21:15:03 +0200

    get_all_teams unfinished

--- src/scinote.jl
+++ src/scinote.jl
@@ -10,28 +10,23 @@ const experiment_id_SEC = "53"
 const task_id_EC = "297"
 const password = "1a2s3d4f"
 const email = "tim.laemmerzahl@uni-due.de"
-
-token_params = Dict(
-    "grant_type" => "password",
-    "client_id" => client_id,
-    "client_secret" => client_secret,
-    "email" => email,
-    "password" => password
-)
 token_header = ["Content-Type" => "application/json"]
 
-token_tim = token()["access_token"]
-header(token) = Dict(
-    "Content-Type" => "application/json",
-    "Authorization" => "Bearer $token"
-)
+
+"""
+Returns the Status of the Scinote API
+"""
+function api_status()
+    r = HTTP.request("GET",SCINOTE_URL*"/api/status",header)
+    JSON.parse(String(r.body))
+end
 
 """
     api_running()
 Returns if the API is running. 
 """
 function api_running()
-    r = HTTP.request("GET",server_name*"/api/health")
+    r = HTTP.request("GET",SCINOTE_URL*"/api/health")
     resp = String(r.body)
     if (resp == "RUNNING") == true
         return true
@@ -40,14 +35,38 @@ function api_running()
     end
 end
 
+"""
+token()
 
+Get token with client_id, client_secret, authorization_code, and redirect_uri.
 """
-Returns the Status of the Scinote API
-"""
-function api_status()
-    r = HTTP.request("GET",server_name*"/api/status",header)
-    JSON.parse(String(r.body))
+function token()
+    if api_running() == true 
+        resp = HTTP.request("POST", SCINOTE_URL*"/oauth/token",token_header,
+        JSON.json(token_params))
+        body = String(resp.body)
+        JSON.parse(body)
+    end
 end
+
+
+
+
+token_params = Dict(
+    "grant_type" => "password",
+    "client_id" => client_id,
+    "client_secret" => client_secret,
+    "email" => email,
+    "password" => password
+)
+
+
+token_tim = token()["access_token"]
+header(token) = Dict(
+    "Content-Type" => "application/json",
+    "Authorization" => "Bearer $token"
+)
+
 
 
 """
@@ -55,23 +74,18 @@ end
 This function retrieves all teams user is member of.   
 """
 function get_all_teams()
-    r = HTTP.request("GET",SCINOTE_URL*"/api/v1/teams",header(token_tim))
+    if api_running() == true 
+    
+        r = HTTP.request("GET",SCINOTE_URL*"/api/v1/teams",header(token_tim))
 
-    body = String(r.body)
-    JSON.parse(body)
+        body = String(r.body)
+        _body = JSON.parse(body)
+        n_teams = length(_body["data"])
+
+        teams = Dict("name" =>_body["data"][i]["attributes"]["name"],"id" => _body["data"][i]["id"] for i in 1:n_teams)
+    end
 end
 
-"""
-token()
-
-Get token with client_id, client_secret, authorization_code, and redirect_uri.
-"""
-function token()
-    resp = HTTP.request("POST", SCINOTE_URL*"/oauth/token",token_header,
-    JSON.json(token_params))
-    body = String(resp.body)
-    JSON.parse(body)
-end
 
 
 
@@ -81,7 +95,7 @@ This function retrieves all projects from the AK Hasselbrink team.
 """
 function get_projects(team::Int64)
     if api_running() == true 
-        resp=HTTP.request("GET",server_name*"/api/v1/teams/$(team)/projects",header)
+        resp=HTTP.request("GET",SCINOTE_URL*"/api/v1/teams/$(team)/projects",header)
         body = String(resp.body)
         data=JSON.parse(body)["data"]
     end 
@@ -93,7 +107,7 @@ This function retrieves all experiments from the specified project
 """
 function get_experiments(team::Int64,project::Int64)
     if api_running() == true 
-        resp=HTTP.request("GET",server_name*"/api/v1/teams/$(team)/projects/$(project)/experiments",header)
+        resp=HTTP.request("GET",SCINOTE_URL*"/api/v1/teams/$(team)/projects/$(project)/experiments",header)
         body = String(resp.body)
         data=JSON.parse(body)["data"] 
     end
@@ -105,7 +119,7 @@ This function retrieves all tasks from a specific experiment.
 """
 function get_tasks(team::Int64,project::Int64,experiment::Int64)
     if api_running() == true 
-        resp=HTTP.request("GET",server_name*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks",header)
+        resp=HTTP.request("GET",SCINOTE_URL*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks",header)
         body = String(resp.body)
         data=JSON.parse(body)["data"]
     end
@@ -118,7 +132,7 @@ This function retrieves all protocols from a specific experiment.
 """
 function get_protocols(team::Int64,project::Int64,experiment::Int64,task::Int64)
     if api_running() == true 
-        resp=HTTP.request("GET",server_name*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks/$(task)/protocols",header)
+        resp=HTTP.request("GET",SCINOTE_URL*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks/$(task)/protocols",header)
         body = String(resp.body)
         data=JSON.parse(body)["data"]
     end
@@ -131,7 +145,7 @@ This function retrieves all steps from a specific protocol.
 """
 function get_steps(team::Int64,project::Int64,experiment::Int64,task::Int64,protocol::Int64)
     if api_running() == true 
-        resp=HTTP.request("GET",server_name*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks/$(task)/protocols/$(protocol)/steps",header)
+        resp=HTTP.request("GET",SCINOTE_URL*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks/$(task)/protocols/$(protocol)/steps",header)
         body = String(resp.body)
         data=JSON.parse(body)["data"]
     end
@@ -144,7 +158,7 @@ Empty cells will be ignored. Be sure to have a proper
 """
 function get_step_table(team::Int64,project::Int64,experiment::Int64,task::Int64,protocol::Int64,step::Int64)
     if api_running() == true
-        resp=HTTP.request("GET",server_name*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks/$(task)/protocols/$(protocol)/steps/$(step)/tables",header)
+        resp=HTTP.request("GET",SCINOTE_URL*"/api/v1/teams/$(team)/projects/$(project)/experiments/$(experiment)/tasks/$(task)/protocols/$(protocol)/steps/$(step)/tables",header)
         body = String(resp.body)
         data=JSON.parse(JSON.parse(body)["data"][1]["attributes"]["contents"])["data"]
 
